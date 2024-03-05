a04b9386e63f4d243a60c79c364b0b9ab8c1e6ce
SandraVLP <aleksanvp@yandex.ru>
2024-03-05 16:03:57 +0300

    usestate

--- 1-todolist/src/App.tsx
+++ 1-todolist/src/App.tsx
@@ -1,29 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import { Todolist } from "./Todolist";
 import { TaskType } from "./Todolist";
 
-
 function App() {
-    let tasks1: Array<TaskType> = [
-      { id: 1, title: 'HTML&CSS', isDone: true },
-      { id: 2, title: 'JS', isDone: true },
-      { id: 3, title: 'ReactJS', isDone: false },
-      { id: 4, title: 'Redux', isDone: false },
-    ]
-   
-    const removeTask = (taskId: number) => {
-      tasks1 = tasks1.filter(task => {
-        return task.id !== taskId
-      })
-      console.log(tasks1)
-    }
-   
-    return (
-      <div className="App">
-        <Todolist title="What to learn" tasks={tasks1} removeTask={removeTask} />
-      </div>
-    )
-  }
+  const [tasks, setTasks] = useState<TaskType[]>([
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false },
+    { id: 4, title: "Redux", isDone: false },
+  ]);
+
+  const removeTask = (taskId: number) => {
+    const filteredTasks = tasks.filter((task) => {
+      return task.id !== taskId;
+    });
+    setTasks(filteredTasks);
+  };
+
+  return (
+    <div className="App">
+      <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} />
+    </div>
+  );
+}
 
 export default App;
